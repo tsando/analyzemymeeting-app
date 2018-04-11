901010ba5585f64507d0ecc1057cc56f25d64ce8
Tanya Sandoval <sandoval.tanya2@gmail.com>
2018-04-11 12:28:47 +0100

    changed formidable for multer to handle form group with file upload and parameters; removed some unused packages

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
+// Require node modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// for file upload
-var formidable = require('formidable');
+// // Parse Cookie header and populate req.cookies
+// var cookieParser = require('cookie-parser');
+// // Generates logs on any requests being made
+// var logger = require('morgan');
+// var formidable = require('formidable');  // for form handly but commented as doesn't work with enctype="multipart/form-data" 
+var multer  = require('multer')
 var fs = require('fs');
+// bodyParser = require('body-parser')  // enables form submissions
 
+// Require routers
 var indexRouter = require('./routes/index');
-
-// Own modules
+// Require own modules
 var run_python = require('./public/js/run_python')
 
 var app = express();
@@ -18,37 +22,38 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Body Parser Middleware
-app.use(logger('dev'));
-// parse application json
-app.use(express.json());
-// parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// Set public folder
+// app.use(logger('dev'));
+// app.use(cookieParser());
+app.use(express.json());  // parse application json
+app.use(express.urlencoded({ extended: false }));  // parse application/x-www-form-urlencoded
+
+// Set static folders
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set routers
 app.use('/', indexRouter);
 
 // -------------------------------------------------------------
 // Upload route
-app.post('/stats', function (req, res) {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-    // `file` is the name of the <input> field of type `file`
-    var oldpath = files.uploaded_file.path;
-    var newpath = path.join(__dirname, '/public/data/uploads/', files.uploaded_file.name);
+var upload = multer({ dest: './public/data/uploads/' })
+app.post('/stats', upload.single('uploaded_file'), function (req, res) {
+  console.log('uploading file')
+  // // var form = new formidable.IncomingForm();
+  // // form.parse(req, function (err, fields, files) {
+    var oldpath = req.file.path;
+    var newpath = path.join(req.file.destination, req.file.originalname);
+    // var newpath = path.join(__dirname, '/public/data/uploads/', files.uploaded_file.name);
     fs.rename(oldpath, newpath, function (err) {
       if (err) throw err;
     })
-    run_python.run_python(__dirname).then(
+    run_python.run_python(__dirname, req.body.nspeakers).then(
       // Must wrap res.render inside function for promise to work
       function () {
         // console.log('After promise complete');
         res.render('stats', { title: 'AnalyzeMyMeeting.com' })
       }
     );
-  })
+  // })
 });
 // -------------------------------------------------------------
 
--- public/js/run_python.js
+++ public/js/run_python.js
@@ -1,13 +1,12 @@
 var path = require('path');
 
-exports.run_python = function (dirname) {
+exports.run_python = function (dirname, nspeakers) {
     // Use a promise so this is done before the 'stats' template is rendered
     return new Promise(function (resolve, reject) {
         console.log('Inside promise')
         var spawn = require('child_process').spawn;
         var input = path.join(dirname, '/public/data/uploads')
         var output = path.join(dirname, '/public/data/csvs')
-        var nspeakers = 4;
         var pyprog = spawn('python', [path.join(dirname, "/public/python/analyzemymeeting.py"), input, nspeakers, output]);
 
         pyprog.stdout.on('data', function (data) {
